f5b220339b72492ffb2443af015e66f88f90e750
Jo1oPedro <joao.pedreira@estudante.ufjf.br>
2025-09-04 22:18:42 -0300

    add consume for external system

--- src/Commands/DownloadCommand.php
+++ src/Commands/DownloadCommand.php
@@ -7,15 +7,21 @@ use Discord\Parts\User\User;
 class DownloadCommand implements Command
 {
     public function __construct(
-        private User $author,
         private string $urlToDownload,
+        private ?User $author = null,
+        private ?string $mediaId = null,
         private $onProgress = "",
         private string $format = "",
     ) {}
     
-    public function getAuthor(): User
+    public function getAuthor(): ?User
     {
         return $this->author;
+    }
+
+    public function getMediaId(): ?String
+    {
+        return $this->mediaId;
     }
 
     public function setFormat(string $format)
